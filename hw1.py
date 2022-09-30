a62d3dfbad49db8165299d5172d81a63623b4e98
Armen <armen_davidyants@mail.ru>
2022-09-30 23:20:01 +0300

    День недели

--- hw1.py
+++ hw1.py
@@ -1,1 +1,10 @@
-hjgfhghgfggbbbfbffffffbbgf
+day_of_week=input("Введите число: ")
+
+if (5< int(day_of_week) <=7):
+    print("Выходной")
+else :
+    if (int(day_of_week)>7 or int(day_of_week)==0):
+        print("error")
+
+    else:
+        print("Рабочий")

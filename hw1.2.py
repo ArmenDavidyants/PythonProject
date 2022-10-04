b63c15b1a7cfc125bf724386a57bb7d4f517f713
Armen <armen_davidyants@mail.ru>
2022-10-04 11:53:01 +0300

    четверть нахождения точки

---
+++ hw1.2.py
@@ -0,0 +1,17 @@
+
+
+x=int (input ("Введите координату x "))
+y=int (input ("Введите координату y "))
+
+if ( x!=0 and  x>0 and  y>0 and  y!=0):
+    print("Первая четверть")
+
+if (x!=0 and  x>0 and  y<0 and  y!=0):
+        print("четвертая четверть")
+
+if (x!=0 and  x<0 and  y>0 and  y!=0):
+    print("Третяя четверть")
+
+if (x!=0 and  x<0 and  y<0 and  y!=0):
+    print("Вторая четверть")
+
